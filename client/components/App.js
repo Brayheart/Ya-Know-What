dcbdc40c96c696bb787deeb6d9487b01fa6d16dd
Justin Kang <JK6@Justins-MacBook-Pro.local>
2017-04-24 13:01:46 -0700

    CDM filter working properly

--- client/components/App.js
+++ client/components/App.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
 import { Form, Button, ButtonControl } from 'react-bootstrap';
+import axios from 'axios';
 
 class App extends Component {
   constructor() {
     super()
 
     this.state = {
-
+      url: [],
+      responses: []
     }
+  }
+
+  componentDidMount() {
+    axios.get('http://www.foaas.com/operations')
+         .then(res => {
+           console.log(res.data)
+           console.log(res.data[1].url.includes(':name' && ':from'));
+           console.log(res.data[2].url.includes(':from' && ':name'));
+           let arr = res.data.filter(data => {
+            //  console.log(data);
+            //  console.log(data.url);
+             return data.url.includes(':name')
+           })
+           console.log(arr);
+         })
   }
 
   render() {
