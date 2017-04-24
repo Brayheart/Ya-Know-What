76678140f2a8819178ab0a877a6f7e8fb94d4216
Justin Kang <JK6@Justins-MacBook-Pro.local>
2017-04-24 15:07:18 -0700

    Added parent app component to handle routing

--- client/index.js
+++ client/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import ParentApp from './components/ParentApp';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<ParentApp />, document.getElementById('app'));
