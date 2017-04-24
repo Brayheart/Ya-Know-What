a5653a35a0b18d6584a67d92967f9dfa2624271a
Justin Kang <JK6@Justins-MacBook-Pro.local>
2017-04-24 14:02:19 -0700

    name and from states successfully logged

--- client/components/App.js
+++ client/components/App.js
@@ -9,8 +9,15 @@ class App extends Component {
 
     this.state = {
       hatefulUrls: [],
-      responses: []
+      lovingUrls: ['/you/:name/:from','/donut/:name/:from', '/shakespeare/:name/:from', '/war/:name/:from', '/slowly/:name/:from', '/aristotle/:name/:from'],
+      responses: [],
+      name: '',
+      from: ''
     }
+
+    this.nameChange = this.nameChange.bind(this);
+    this.fromChange = this.fromChange.bind(this);
+    // this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
@@ -22,9 +29,24 @@ class App extends Component {
            _.each(urls, url => {
              this.setState({hatefulUrls: [...this.state.hatefulUrls, url.url]})
            })
-           console.log(this.state.hatefulUrls)
          })
   }
+
+  nameChange(event) {
+    this.setState({name: event.target.value});
+    console.log(this.state.name)
+  }
+  
+  fromChange(event) {
+    this.setState({from: event.target.value});
+    console.log(this.state.from);
+  }
+
+  // handleSubmit(event){
+  //   event.preventDefault();
+  //   axios.get('http://www.foaas.com/' + this.state.hatefulUrls[Math.floor(Math.random()*this.state.hatefulUrls.length)] + )
+
+  // }
 
   render() {
     return(
@@ -32,10 +54,10 @@ class App extends Component {
         <h1>Ya Know What?!</h1>
         <br/>
         <Form>
-          <input type="text" name="name" placeholder="  Who Needs To Know??"/>
+          <input onChange={this.nameChange} type="text" name="name" placeholder="  Who Needs To Know??"/>
           <br/>
           <br/>
-          <input type="text" name="from" placeholder="    Who's Telling Em??"/>
+          <input onChange={this.fromChange} type="text" name="from" placeholder="    Who's Telling Em??"/>
         </Form>
           <br/>
         <Button bsStyle="primary" type="submit">
