0443f8d89105b820f939062796b11de0cc2e8081
Justin Kang <JK6@Justins-MacBook-Pro.local>
2017-04-24 11:34:53 -0700

    Implemented routes. Auto-Increment working correctly

--- server/server.js
+++ server/server.js
@@ -3,9 +3,19 @@ const path = require('path');
 const PORT = 3000;
 const db = require('./db/db');
 
+const router = require('./router');
+const parser = require('body-parser');
+
 const app = express();
 
+app.use(parser.json());
+app.use(parser.urlencoded({
+  extended: true
+}))
+
 app.use(express.static(path.join(__dirname, 'client')));
+
+app.use('/', router);
 
 app.listen(PORT, err => {
   if(err) {
