13b0339bc1402940f101bfcbc668739aa043ceb1
Justin Kang <JK6@Justins-MacBook-Pro.local>
2017-04-25 10:23:54 -0700

    added css loader and style loader

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,11 @@ module.exports = {
           loader: 'babel-loader',
           options: { presets: ['es2015', 'react'] }
         }],
-      }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
     ]
   }
 }
